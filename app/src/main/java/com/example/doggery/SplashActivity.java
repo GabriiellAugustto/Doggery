d93bee8b3e8e1a9a435b9eabb2ef893f617fe8d0
GabriiellAugustto <gabriel.gauitarra.05@gmail.com>
2023-08-08 02:03:35 -0300

    Quarto
     Salve

--- app/src/main/java/com/example/doggery/SplashActivity.java
+++ app/src/main/java/com/example/doggery/SplashActivity.java
@@ -1,17 +1,43 @@
 package com.example.doggery;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.dynamicanimation.animation.DynamicAnimation;
+import androidx.viewpager2.widget.ViewPager2;
 
+import android.animation.ObjectAnimator;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.FileUtils;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+import java.io.ObjectInput;
+import java.util.Objects;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.logging.LogRecord;
 
 public class SplashActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_layout);
 
-        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                finish();
+
+            }
+        },3000);
+
+
     }
 }
