99d55dff415c36f6a85d532c8daf4ff4247c0bb1
GabriiellAugustto <gabriel.gauitarra.05@gmail.com>
2023-09-11 16:36:23 -0300

    Prototipo1.2

---
+++ app/src/main/java/com/example/doggery/AgendaActivity.java
@@ -0,0 +1,13 @@
+package com.example.doggery;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AgendaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.agenda_layout);
+    }
+}
--- app/src/main/java/com/example/doggery/HomeFragment.java
+++ app/src/main/java/com/example/doggery/HomeFragment.java
@@ -1,21 +1,58 @@
 package com.example.doggery;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 
 public class HomeFragment extends Fragment {
 
-    HomeFragment(){}
+    HomeFragment() {
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        ImageButton imageButton=(ImageButton) view.findViewById(R.id.btnpasseio);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity().getApplicationContext(), PasseioActivity.class));
+            }
+        });
+
+
+        ImageButton imageButton1=(ImageButton) view.findViewById(R.id.btnhotel);
+        imageButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity().getApplicationContext(), HotelActivity.class));
+            }
+        });
+
+        ImageButton imageButton2=(ImageButton) view.findViewById(R.id.btnagenda);
+        imageButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity().getApplicationContext(), AgendaActivity.class));
+            }
+        });
+
+
+     return view;
+
     }
+
 }
